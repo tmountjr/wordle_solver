ce5b973062b152757060bcfae6a5ceba80af39b1
Tom Mount <tmountjr@gmail.com>
2022-02-18 14:51:31 -0500

    update word list to provide a readonly list of words as a property

--- WordList.ts
+++ WordList.ts
@@ -2,7 +2,7 @@ import { union, difference, intersect } from './helpers';
 
 export class WordList {
   protected groupings: Map<string, Set<string>>[] = [];
-  protected words: Set<string>;
+  protected wordPool: Set<string>;
   public size: number = 0;
   private readonly wordLength: number = 0;
   protected wordsWithLetter = new Map<string, Set<string>>();
@@ -10,7 +10,7 @@ export class WordList {
   constructor(words: string[]) {
     this.wordLength = words[0].length;
     if (!words.every(w => w.length === this.wordLength)) throw new Error('All words must be the same length.');
-    this.words = new Set<string>(words);
+    this.wordPool = new Set<string>(words);
     this.regenerateList();
   }
 
@@ -20,7 +20,7 @@ export class WordList {
    */
   public removeLetter(letter: string): void {
     const wordsWith = this.containsAny(letter);
-    this.words = difference(this.words, wordsWith);
+    this.wordPool = difference(this.wordPool, wordsWith);
     this.regenerateList();
   }
 
@@ -29,7 +29,7 @@ export class WordList {
    * @param letter The letter to search for.
    */
   public containsAny(letter: string): Set<string> {
-    if (this.words.size === 0) throw new Error('Word list is empty.');
+    if (this.wordPool.size === 0) throw new Error('Word list is empty.');
     return this.wordsWithLetter.get(letter) || new Set<string>();
   }
 
@@ -39,7 +39,7 @@ export class WordList {
    * @param index The index at which to search.
    */
   public containsAtIndex(letter: string, index: number): Set<string> {
-    if (this.words.size === 0) throw new Error('Word list is empty.');
+    if (this.wordPool.size === 0) throw new Error('Word list is empty.');
     return this.groupings[index].get(letter) || new Set<string>();
   }
 
@@ -62,9 +62,9 @@ export class WordList {
     if (details['x'].length) {
       const toRemove = union(...details['x'].map(i => guess[i]).map(letter => this.wordsWithLetter.get(letter) || new Set<string>()));
       if (toRemove.size > 0) {
-        const prevWordsSize = this.words.size;
-        this.words = difference(this.words, toRemove);
-        const afterWordsSize = this.words.size;
+        const prevWordsSize = this.wordPool.size;
+        this.wordPool = difference(this.wordPool, toRemove);
+        const afterWordsSize = this.wordPool.size;
         if (prevWordsSize !== afterWordsSize) this.regenerateList();
       }
     }
@@ -73,10 +73,10 @@ export class WordList {
     if (details['g'].length) {
       const mustHave = intersect(...details['g'].map(position => this.containsAtIndex(guess[position], position)));
       if (mustHave.size > 0) {
-        const prevWordsSize = this.words.size;
-        this.words = intersect(this.words, mustHave);
+        const prevWordsSize = this.wordPool.size;
+        this.wordPool = intersect(this.wordPool, mustHave);
         this.regenerateList();
-        const afterWordsSize = this.words.size;
+        const afterWordsSize = this.wordPool.size;
         if (prevWordsSize !== afterWordsSize) this.regenerateList();
       }
     }
@@ -86,9 +86,9 @@ export class WordList {
       let mustHave = intersect(...details['y'].map(i => this.containsAny(guess[i])));
       mustHave = difference(mustHave, ...details['y'].map(i => this.containsAtIndex(guess[i], i)));
       if (mustHave.size > 0) {
-        const prevWordsSize = this.words.size;
-        this.words = intersect(this.words, mustHave);
-        const afterWordsSize = this.words.size;
+        const prevWordsSize = this.wordPool.size;
+        this.wordPool = intersect(this.wordPool, mustHave);
+        const afterWordsSize = this.wordPool.size;
         if (prevWordsSize !== afterWordsSize) this.regenerateList();
       }
     }
@@ -100,14 +100,14 @@ export class WordList {
    * @returns A word from the list.
    */
   public getRandomWord(...mustContain: string[]): string {
-    if (this.words.size > 0) {
+    if (this.wordPool.size > 0) {
       let pool: Set<string>;
       if (mustContain.length > 0) {
         pool = intersect(...mustContain.map(letter => this.containsAny(letter)));
       } else {
-        pool = this.words;
+        pool = this.wordPool;
       }
-      return [...pool][Math.floor(Math.random() * this.words.size)];
+      return [...pool][Math.floor(Math.random() * this.wordPool.size)];
     }
     throw new Error('Cannot get random word from empty list.');
   }
@@ -118,7 +118,7 @@ export class WordList {
   private regenerateList(): void {
     this.groupings = [];
     this.wordsWithLetter = new Map<string, Set<string>>();
-    for (let word of this.words) {
+    for (let word of this.wordPool) {
       for (let i = 0; i < this.wordLength; i++) {
         // Populate the groupings object.
         if (this.groupings.length !== this.wordLength) this.groupings.push(new Map<string, Set<string>>());
@@ -139,6 +139,13 @@ export class WordList {
     }
 
     // Update the size property.
-    this.size = this.words.size;
+    this.size = this.wordPool.size;
+  }
+
+  /**
+   * Get a list of available words (read-only).
+   */
+  get words(): string[] {
+    return [...this.wordPool];
   }
 }
